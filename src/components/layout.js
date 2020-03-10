82dba5207aee05573e3fd4e9afbb28c0cec8a678
James Hooper <jameshooperdesigns@gmail.com>
2020-03-09 22:54:18 -0600

    Make changes to images

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+
+
 import Header from "./header"
 import "./layout.css"
 
@@ -39,6 +41,7 @@ const Layout = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
+  
       </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,15 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import DownLoadsPage from "../components/downloads"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>This is Gatsby!</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <div class="character">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/exgoaU3Fr5E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <DownLoadsPage className="flex" />
 
-    </div>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
